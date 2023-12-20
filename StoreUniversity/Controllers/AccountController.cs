709a41014c21e7a9a4a76c93cae87ad9de3548e3
shayan <shayanharati8@gmail.com>
2023-12-20 18:57:26 +0330

    register view maked

---
+++ StoreUniversity/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreUniversity.DTOs;
+
+namespace StoreUniversity.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/register")]
+        public IActionResult Register()
+        {
+            RegisterViewModel vm= new RegisterViewModel();
+            return View(vm);
+        }
+        [HttpPost]
+        [Route("/register")]
+        public IActionResult Register(RegisterViewModel vm)
+        {
+            
+            return View(vm);
+        }
+    }
+}
---
+++ StoreUniversity/DTOs/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreUniversity.DTOs
+{
+    public class RegisterViewModel
+    {
+        [Display(Name ="نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد  کنید")]
+        public string UserName { get; set; }
+        [Display(Name ="رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد  کنید")]
+        public string Password { get; set; } 
+        [Display(Name ="تکرار رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد  کنید")]
+        [Compare("Password",ErrorMessage ="{0} با {1} یکسان نیست")]
+        public string RePassword { get; set; }
+        [Display(Name ="ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد  کنید")]
+        [EmailAddress(ErrorMessage ="{0} وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+    }
+}
